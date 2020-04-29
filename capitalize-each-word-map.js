eaef91f0e2f6e5daa8a1d0e7c8e570947972b80c
Will Humphreys <whumphreys@gmail.com>
2020-04-29 23:00:45 +0200

    Add capitalize using map

---
+++ capitalize-each-word-map.js
@@ -0,0 +1,7 @@
+function capitalizeMap(text) {
+  const words = text.split(" ");
+
+  return words.map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
+console.log(`Each word capitalized ${capitalizeMap("there are some words")}`);
