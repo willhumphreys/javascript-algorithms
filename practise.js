aa68d96b053b4a4820f001213eb41a9a99954622
Will Humphreys <whumphreys@gmail.com>
2020-04-29 22:56:54 +0200

    Add capitalize map

--- practise.js
+++ practise.js
@@ -4,37 +4,37 @@ function reverse(text) {}
 
 console.log(`1: Reverse one line:  ${reverse("Hello")}`);
 
-// reverse one line spread
+// 2 reverse one line spread
 
 function reverseSpread(text) {}
 
 console.log(`2: Reverse spread: ${reverseSpread("Hello")}`);
 
-// reverse for loop
+// 3 reverse for loop
 
 function reverseLoop(text) {}
 
 console.log(`3: Reverse loop: ${reverseLoop("Hello")}`);
 
-// reverse short for loop
+// 4 reverse short for loop
 
 function reverseLoopShort(text) {}
 
 console.log(`4: Reverse loop short: ${reverseLoopShort("Hello")}`);
 
-// reverse recursion
+// 5 reverse recursion
 
 function reverseRecursion(text) {}
 
 console.log(`5: Reverse recursion: ${reverseRecursion("Hello")}`);
 
-// reverse reduce
+// 6 reverse reduce
 
 function reverseReduce(text) {}
 
 console.log(`6: Reverse reduce: ${reverseReduce("Hello")}`);
 
-// count vowels iterative
+// 7 count vowels iterative
 
 const vowels = ["a", "e", "i", "o", "u"];
 
@@ -42,17 +42,19 @@ function countVowels(text) {}
 
 console.log(`7: Count vowels: ${countVowels("Hello")}`);
 
-// count vowels regex
+// 8 count vowels regex
 
 function countVowelsRegex(text) {}
 
 console.log(`8: Count vowels regex: ${countVowelsRegex("Hello")}`);
 
-// find the most recurring character character map
+// 9 find the most recurring character character map
 
 function mostRecurringForIn(text) {}
 
 console.log(`9: Most recurring letter: ${mostRecurringForIn("Hello")}`);
+
+// 10 find the most recurring letter using arrays
 
 function mostRecurringUsingArrays(text) {}
 
@@ -60,6 +62,14 @@ console.log(
   `10: Most recurring using Arrays ${mostRecurringUsingArrays("Hello")}`
 );
 
+// 11 Capitalize each letter using for
+
 function capitalizeFor(text) {}
 
 console.log(`11 Capitalize words for:  ${capitalizeFor("hello. how are you")}`);
+
+// 12 Capitalize each letter using map
+
+function capitalizeMap(text) {}
+
+console.log(`12 Capitalize words for:  ${capitalizeMap("hello. how are you")}`);
